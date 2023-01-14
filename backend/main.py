eba883b1539bd7132c0845ec04c19cc8c2abb328
zackjmccann <zackjmccann@gmail.com>
2023-01-14 12:57:30 -0700

    fresh start

--- backend/main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    """Entry to the backend"""
-    print('I am the backend')
-
-if __name__ == '__main__':
-    main()
-
--- client/main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    """Client Entry"""
-    print('I am the client')
-
-if __name__ == '__main__':
-    main()
-
